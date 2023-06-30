0f668ff2152ceed14ae3cc9d11a262dad3bb8567
athulpm <97athulpm@gmail.com>
2023-06-30 10:02:43 +0530

    Simple backup script

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Paths
+source_directory="C:\Users\97ath\OneDrive\Desktop\Bash"
+backup_directory="C:\Users\97ath\OneDrive\Desktop\Bash\backup"
+
+# Backup command
+cp -r "$source_directory" "$backup_directory"
---
+++ readme.md
@@ -0,0 +1,4 @@
+After creating backup script, give it execution permission:
+chmod +x backup.sh
+
+Run the script: backup.sh
